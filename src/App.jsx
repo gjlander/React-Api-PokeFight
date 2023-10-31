0b6f81c1b5d8c8d1b05bf81fcf784e5082aca6e3
RockandRolls <livelydev010@gmail.com>
2023-10-31 17:43:52 +0100

    background component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-// import { useState } from 'react'
+import { useState,useEffect} from 'react';
+import {Routes,Route} from "react-router-dom"
+// import TrendingPage from './pages/TrendingPage';
+// import LandingPage from "./pages/LandingPage";
+// import LoginPage from './pages/LoginPage';
+// import SignUpPage from './pages/SignUpPage';
+// import NotFound from './pages/NotFound.jsx';
+// import Footer from './components/Footer';
+// import 'react-multi-carousel/lib/styles.css';
+// import NavBar from './components/NavBar';
+import Background from './components/Background';
+
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <NavBar />
-      <Routes>  
+      {/* {/* <NavBar /> */}
+      <Background/> 
+      {/* <Routes>  
         <Route path='/' element={<LandingPage />} />  
       </Routes>
-      <Footer/>*/}
+      <Footer/>*/} 
     </>
   )
 }
